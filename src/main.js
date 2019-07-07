8bcf504eb737356cc7bf6b0e96daf617b860b2da
风居住过的街道 <clncon@163.com>
2019-07-07 18:46:14 +0800

    init sell project

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import goods from 'components/goods/goods';
+import sells from 'components/sells/sells';
+import ratings from 'components/ratings/ratings';
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.config.productionTip = false;
+/* eslint-disable no-new */
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/sells', component: sells}
+];
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
+// router.push({path: '/goods'});
